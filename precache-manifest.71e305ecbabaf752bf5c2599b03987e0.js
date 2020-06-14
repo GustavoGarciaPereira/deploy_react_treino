4e1de23ac0852247f65405e717d01669a3a6e483
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-06-14 12:58:51 -0300

    Updates

---
+++ precache-manifest.71e305ecbabaf752bf5c2599b03987e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893ed8b10c0e4803406be5409349c358",
+    "url": "/deploy_react_treino/index.html"
+  },
+  {
+    "revision": "41f3fb33b7dfa9c970c5",
+    "url": "/deploy_react_treino/static/css/main.aeda7719.chunk.css"
+  },
+  {
+    "revision": "14a8f6ccb45bc4271a44",
+    "url": "/deploy_react_treino/static/js/2.c099c141.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy_react_treino/static/js/2.c099c141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f3fb33b7dfa9c970c5",
+    "url": "/deploy_react_treino/static/js/main.fb7a60f9.chunk.js"
+  },
+  {
+    "revision": "1a571b001f5e1e744c12",
+    "url": "/deploy_react_treino/static/js/runtime-main.f115b064.js"
+  },
+  {
+    "revision": "f234e6a39f0b3194481ae29ae5600a43",
+    "url": "/deploy_react_treino/static/media/gm.f234e6a3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy_react_treino/static/media/logo.5d5d9eef.svg"
+  }
+]);
